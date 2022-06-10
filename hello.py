934efe2eaaa2bedfa08326789a4acc3c7d7eb977
konrad1208 <32593523+konrad1208@users.noreply.github.com>
2022-06-10 11:51:02 +0100

    Fixed conflict

--- hello.py
+++ hello.py
@@ -1,5 +1,23 @@
 def david():
     print("Hello David!")
 
+<<<<<<< HEAD
 def konrad():
     print("Hello Konrad!")
+=======
+
+def harry():
+    print("Hello Harry!")
+    
+    
+def greeting(first_name):
+    print('Hello', first_name)
+
+    
+greeting('James')
+
+
+
+
+
+>>>>>>> origin/main
